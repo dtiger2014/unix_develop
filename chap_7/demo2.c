cf437418a9e8790077c4bafc83932fa0556c1168
echoFanFan <dtiger2014@163.com>
2020-07-16 08:26:48 +0800

    chap 7

---
+++ chap_7/demo2.c
@@ -0,0 +1,14 @@
+/* 命令行参数 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    for (i = 0; i < argc; i++) // echo all command-line args
+        printf("argv[%d]: %s\n", i, argv[i]);
+    
+    exit(0);
+}
